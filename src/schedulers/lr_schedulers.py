5c06a580cfe3520f6d0faf9249a977033a517bf6
= <kirklimushin@gmail.com>
2024-02-26 10:53:13 +0300

    Added folder for storing implementations of LR Schedulers

---
+++ src/schedulers/lr_schedulers.py
@@ -0,0 +1,13 @@
+from torch.optim.lr_scheduler import LRScheduler
+
+class MultiStepLR(LRScheduler):
+    pass 
+
+class PolyLR(LRScheduler):
+    pass 
+
+class ReduceLROnPlateau(LRScheduler):
+    pass 
+
+class ExponentialLR(LRScheduler):
+    pass 

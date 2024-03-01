e0e4b556c7f9db525a506f51af3a25e0d64f1159
= <kirklimushin@gmail.com>
2024-03-01 11:45:47 +0300

    Tweaked REST API settings:

--- src/rest/settings.py
+++ src/rest/settings.py
@@ -1,12 +1,54 @@
-import fastapi 
-from src.rest import controllers
-import os 
+import fastapi
+import os
+import logging
 
-DEBUG = os.environ.get("DEBUG", False)
-application = fastapi.FastAPI(debug=DEBUG)
-
-application.add_api_route(
-    path="/remove/face/details",
-    endpoint=controllers.remove_face_from_image
+from src.rest.controllers import (
+    remove_face_from_image,
+    healthcheck,
+    parse_system_metrics
 )
+
+from fastapi.middleware import cors
+
+logger = logging.getLogger("startup_logger")
+file_handler = logging.FileHandler(filename="startup_log.log")
+logger.addHandler(file_handler)
+
+VERSION = os.environ.get("VERSION", "1.0.0")
+ALLOWED_METHODS = os.environ.get("ALLOWED_METHODS", "*")
+ALLOWED_HEADERS = os.environ.get("ALLOWED_HEADERS", "*")
+ALLOWED_ORIGINS = os.environ.get("ALLOWED_ORIGINS", "*")
+
+application = fastapi.FastAPI(version=VERSION)
+
+try:
+    application.add_middleware(
+        middleware_class=cors.CORSMiddleware,
+        allow_origins=ALLOWED_ORIGINS,
+        allow_headers=ALLOWED_HEADERS,
+        allow_methods=ALLOWED_METHODS,
+    )
+except (Exception) as err:
+    logger.error("failed to add core middlewares for protecting application")
+
+try:
+    application.add_api_route(
+        path='/api/remove/facial/details/{facial_method}/{media_type}',
+        endpoint=remove_face_from_image,
+        methods=['POST'],
+    )
+    application.add_api_route(
+        path='/healthcheck/',
+        endpoint=healthcheck,
+        methods=['GET']
+    )
+    application.add_api_route(
+        path="/api/system/metrics/",
+        endpoint=parse_system_metrics,
+        methods=["GET"],
+    )
+
+except (Exception) as err:
+    logger.fatal("failed to start ASGI Server application")
+    raise SystemExit("Failed to start application, check logs.")
 
